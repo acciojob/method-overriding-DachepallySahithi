3d45c967969477ca6a1eeb4995f0b881bfd16ccd
DachepallySahithi <dachepallysahithi@gmail.com>
2024-11-13 18:18:19 +0530

    added extra lines

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,21 @@
 package com.driver;
 
-class A{
-    public String meth(){
+class A {
+    public String meth() {
         return "Invoking method from class A";
     }
 }
-class B extends A{
-    public String meth(){
+class B extends A {
+    @Override
+    public String meth() {
         return "Method is overridden in Extended class B";
     }
 }
 public class Main {
     public static void main(String[] args){
-        B obj = new B();
-        System.out.println("Calling meth from object of B: " + obj.meth());
-        System.out.println("Calling meth from A using object of B: " + obj.super.meth());
+        A.objA = new A();
+        System.out.println(objA.meth());
+        B objB = new B();
+        System.out.println(objB.meth());
     }
-  
 }
