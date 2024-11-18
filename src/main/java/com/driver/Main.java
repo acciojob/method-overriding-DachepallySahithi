f8e163c8e1238d810254c142bc6b34a3eba8b9cd
DachepallySahithi <dachepallysahithi@gmail.com>
2024-11-18 18:02:45 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,15 +9,12 @@ public class A {
 public class B extends A{
     @Override
     public static String meth() {
-        return "Method is overridden in Extendend class B;
+        return "Method is overridden in Extendend class B";
     }
 }
 public class Main {
     public static void main(String[] args){
         B obj = new B();
         System.out.println(obj.meth());
-
-//        B b1 = new B();
-//        b1.meth();
     }
 }
