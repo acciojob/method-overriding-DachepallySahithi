af624c3112310d4f4787e973406e7c8e337ffd0d
DachepallySahithi <dachepallysahithi@gmail.com>
2024-11-25 09:51:36 +0530

    added extra line

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,21 @@
 package com.driver;
 
-
-class A {
+class A{
     public String meth(){
         return "Invoking method from class A";
     }
 }
-class B extends A{
-   @Override
 
+class B extends A{
+    @java.lang.Override
     public String meth() {
-        return "Method is overridden in Extendend class B";
+        return "Method is overridden in Extended class B";
+        return super.meth();
     }
 }
 public class Main {
     public static void main(String[] args){
         B obj = new B();
-        System.out.println(obj.super.meth());
         System.out.println(obj.meth());
     }
 }
