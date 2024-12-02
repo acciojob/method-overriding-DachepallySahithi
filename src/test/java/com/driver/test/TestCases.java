b523d7b420482a307850395457d5467a44ddc230
DachepallySahithi <dachepallysahithi@gmail.com>
2024-12-02 10:24:19 +0530

    Testing

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -6,3 +6,16 @@ package com.driver.test;
 public class TestCases {
     
 }
+
+class A {
+    public String meth(){
+        return "Invoking method from class A";
+
+    }
+}
+
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extended class B";
+    }
+}
