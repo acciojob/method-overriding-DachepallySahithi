10fe9873be2e480b05625a5d3be015e2b9241709
DachepallySahithi <dachepallysahithi@gmail.com>
2024-12-02 11:25:55 +0530

    added extra lines

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,10 +8,12 @@ public class Main {
     }
 
     public static class B extends A{
+        @Override
         public String meth(){
             return "Method is overridden in Extended class B";
         }
     }
+
         public static void main(String[] args){
             B b = new B();
             System.out.println(b.meth());
