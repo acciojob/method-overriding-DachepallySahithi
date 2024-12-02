f2aab5248c4b72cf62de9354a37399cd6e0cae63
DachepallySahithi <dachepallysahithi@gmail.com>
2024-12-02 11:18:36 +0530

    added lines

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,20 @@
 package com.driver;
 
 public class Main {
-    public static void main(String args[]) {
-        B b = new B();
-        b.meth();
-        b.meth();
+    public static class A {
+        public String meth(){
+            return "Invoking method from class A";
+        }
     }
-}
+
+    public static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extended class B";
+        }
+    }
+        public static void main(String[] args){
+            B b = new B();
+            System.out.println(b.meth());
+            System.out.println(b.meth());
+        }
+    }
