dc8132f0e9acc1653a5a3f4026230493e5be834c
DachepallySahithi <dachepallysahithi@gmail.com>
2024-12-02 10:29:27 +0530

    Initial Code

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,21 +1,8 @@
 package com.driver.test;
 
-// import static org.junit.Assert.*;
-// import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    
-}
 
-class A {
-    public String meth(){
-        return "Invoking method from class A";
-
-    }
-}
-
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extended class B";
-    }
 }
